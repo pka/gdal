5a4aab3327ece0d52daf315323144b40f7d7b9d3
Pirmin Kalberer <pka@sourcepole.ch>
2014-01-10 16:39:04 +0100

    IlisMeta model reader class skeleton

---
+++ gdal/ogr/ogrsf_frmts/ili/imdreader.cpp
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  Interlis 1/2 Translator
+ * Purpose:  IlisMeta model reader.
+ * Author:   Pirmin Kalberer, Sourcepole AG
+ *
+ ******************************************************************************
+ * Copyright (c) 2014, Pirmin Kalberer, Sourcepole AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+
+#include "imdreader.h"
+#include "cpl_minixml.h"
+
+
+CPL_CVSID("$Id$");
+
+
+ImdReader::ImdReader() {
+    fpImd = NULL;
+}
+
+ImdReader::~ImdReader() {
+    if (fpImd) VSIFClose( fpImd );
+}
+
+int ImdReader::ReadModel(const char *pszFilename) {
+    fpImd = VSIFOpen( pszFilename, "r" );
+    if( fpImd == NULL )
+    {
+        CPLError( CE_Failure, CPLE_OpenFailed,
+                    "Failed to open IlisMeta file `%s'.",
+                    pszFilename );
+
+        return FALSE;
+    }
+    return TRUE;
+}
